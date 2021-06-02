23bc32380cb977b86f1ca44bc9aac9fcd2d1bd98
nond <nondxm@gmail.com>
2021-06-02 16:49:36 +0300

    [bf] 0602.00  - added input file parser to ut.py (not tested yet)

--- bf.py
+++ bf.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
+import ut
+
+
+def main():
+    mem_strip = ut.MemoryStrip()
+    code_strip = ut.input_file()
+
--- ut.py
+++ ut.py
@@ -6,6 +6,20 @@ DEFAULT_VERSION = '0601.00'
 DEFAULT_AUTHOR = 'nonD'
 RESOLUTION = (1024, 768)
 INIT_WINDOW_POS = (125, 75)
+MEM_MAX_VALUE = 256
+STRIP_LENGTH = 32
+
+
+def input_file(filename: str = 'code.txt') -> (str, str):  # TODO: optimize using regex
+    s = ''
+    s_cut = ''
+    with open(filename, mode='r', encoding='utf8') as infile:
+        s = infile.read()
+        fit_chars = '+-[]<>.,'
+        for c in s:
+            if c in fit_chars:
+                s_cut += c
+    return s, s_cut
 
 
 class TitleInfo:
@@ -22,3 +36,37 @@ class TitleInfo:
             self.title = '{} v.{} by {}'.format(name, ver, author)
         else:
             self.title = title_override
+
+
+class MemoryStrip:
+
+    def __init__(
+            self,
+            length: int = STRIP_LENGTH
+    ):
+        self.length = length
+        self.pos = 0
+        self.strip = []
+        for i in range(self.length):
+            self.strip.append(0)
+
+    def add(self, operand: int = 0):
+        self.strip[self.pos] = (self.strip[self.pos] + operand) % MEM_MAX_VALUE
+
+    def sub(self, operand: int = 0):
+        self.add(-operand)
+
+    def inc(self):
+        self.add(1)
+
+    def dec(self):
+        self.add(-1)
+
+    def move(self, operand: int = 0):
+        self.pos = (self.pos + operand) % self.length
+
+    def left(self, operand: int = 0):
+        self.move(-operand)
+
+    def right(self, operand: int = 0):
+        self.move(operand)
